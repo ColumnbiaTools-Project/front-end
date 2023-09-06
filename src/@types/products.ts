1bc690d098c1f83562fd45ecbeb165c3c913adb0
pelicanStd <pelican8118@gmail.com>
2023-09-06 20:52:29 +0900

    feat:cartPage setting

--- src/@types/products.ts
+++ src/@types/products.ts
@@ -1,6 +1,6 @@
-export type Products = Product[];
+declare type Products = Product[];
 
-export interface Product {
+declare interface Product {
   id?: string;
   title: string;
   price: string;
@@ -14,3 +14,9 @@ export interface Product {
 // price는 string으로 저장되어야함. number로 저장하면 0이 사라짐
 // id는 add할때는 자동으로 들어가서 optional로 설정
 
+declare interface CartProduct {
+  title: string;
+  price: number;
+  color: string;
+  id: string;
+}
