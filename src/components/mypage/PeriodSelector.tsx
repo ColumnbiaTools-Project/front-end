c08f35ed69a61b7c351459c43a805382b3314352
KimGyeongwon <ruddnjs6651@gmail.com>
2023-09-12 19:54:48 +0900

    feat : PeriodSelector 추가

--- src/components/mypage/PeriodSelector.tsx
+++ src/components/mypage/PeriodSelector.tsx
@@ -1,8 +1,19 @@
 "use client";
 export default function PeriodSelector() {
   return (
-    <div>
-      <div>기간별 선택!!</div>
+    <div className="flex justify-center gap-6">
+      <div className="flex gap-1 items-center">
+        <p>기간별</p>
+        <button className="btn">3개월</button>
+        <button className="btn">6개월</button>
+        <button className="btn">1년</button>
+      </div>
+      <form className="flex gap-1 items-center" action="">
+        <p>캘린더</p>
+        <input type="date" />
+        <p>~</p>
+        <input type="date" />
+      </form>
     </div>
   );
 }
