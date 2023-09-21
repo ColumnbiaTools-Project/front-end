985734b85e9f25ae1d27667673f2397db82ef4c2
KimGyeongwon <ruddnjs6651@gmail.com>
2023-09-21 21:58:07 +0900

    feat: orderId 추가 및 css nesting 오류 수정중

--- src/app/mypage/orders/[orderId]/page.tsx
+++ src/app/mypage/orders/[orderId]/page.tsx
@@ -1,3 +1,5 @@
+import DetailOrder from "@/components/mypage/DetailOrder";
+
 export default function OrderDetailpage({
   params,
 }: {
@@ -7,6 +9,7 @@ export default function OrderDetailpage({
     <div>
       page
       <div>params.orderId : {params.orderId}</div>
+      <DetailOrder orderId={params.orderId} userId="1234test" />
     </div>
   );
 }
