deb27f09742d16398d879158a3394b060c32f5be
KimGyeongwon <ruddnjs6651@gmail.com>
2023-10-04 19:11:24 +0900

    feat: 중간커밋

--- src/app/mypage/layout.tsx
+++ src/app/mypage/layout.tsx
@@ -1,11 +1,17 @@
 import SideBar from "@/components/mypage/SideBar";
 import OrderProgress from "@/components/mypage/OrderProgress";
+import { auth } from "@/services/firebase/config";
+import { redirect } from "next/dist/server/api-utils";
+import { NextApiResponse } from "next";
 
 export default function MyPageLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const user = auth.currentUser;
+  console.log("user", user);
+
   return (
     <section className="w-[1280px] mx-auto mb-[200px]">
       <header className="border-b-2 w-full border-whitegray h-[304px] mb-24 box-border flex justify-start items-end">
