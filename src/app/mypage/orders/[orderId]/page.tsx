e5ac560c83f9db1d922030e8ce40fc05c6906ac1
KimGyeongwon <ruddnjs6651@gmail.com>
2023-10-05 17:55:14 +0900

    feat: 수정

--- src/app/mypage/orders/[orderId]/page.tsx
+++ src/app/mypage/orders/[orderId]/page.tsx
@@ -1,28 +1,15 @@
 import DetailOrder from "@/components/mypage/DetailOrder";
-import { getOrders } from "@/services/firebase/orders";
-import { GetStaticProps, GetStaticPropsContext } from "next";
-import { ParsedUrlQuery } from "querystring";
 
-interface OrderDetailPageProps {
-  order: Order;
-}
-interface OrderDetailPageParams extends ParsedUrlQuery {
-  orderId: string;
-}
-
-export const getStaticProps = async ({ params }: { params: any }) => {
-  const orderId = params?.orderId as string;
-  const order = await getOrders(orderId);
-
-  return { props: { order } };
-};
-
-export default function OrderDetailPage({ order }: OrderDetailPageProps) {
+export default function OrderDetailpage({
+  params,
+}: {
+  params: { orderId: string };
+}) {
   return (
     <div>
       page
-      <div>params.orderId : {order.id}</div>
-      <DetailOrder orderId={order.id!} userId="1234test" />
+      <div>params.orderId : {params.orderId}</div>
+      <DetailOrder orderId={params.orderId} userId="1234test" />
     </div>
   );
 }
