934f511c727094f9d2942b7da827b44e5c75d6b6
KimGyeongwon <ruddnjs6651@gmail.com>
2023-10-05 17:43:21 +0900

    ㅜㅠ

--- src/app/mypage/orders/[orderId]/page.tsx
+++ src/app/mypage/orders/[orderId]/page.tsx
@@ -1,15 +1,28 @@
 import DetailOrder from "@/components/mypage/DetailOrder";
+import { getOrders } from "@/services/firebase/orders";
+import { GetStaticProps, GetStaticPropsContext } from "next";
+import { ParsedUrlQuery } from "querystring";
 
-export default function OrderDetailpage({
-  params,
-}: {
-  params: { orderId: string };
-}) {
+interface OrderDetailPageProps {
+  order: Order;
+}
+interface OrderDetailPageParams extends ParsedUrlQuery {
+  orderId: string;
+}
+
+export const getStaticProps = async ({ params }: { params: any }) => {
+  const orderId = params?.orderId as string;
+  const order = await getOrders(orderId);
+
+  return { props: { order } };
+};
+
+export default function OrderDetailPage({ order }: OrderDetailPageProps) {
   return (
     <div>
       page
-      <div>params.orderId : {params.orderId}</div>
-      <DetailOrder orderId={params.orderId} userId="1234test" />
+      <div>params.orderId : {order.id}</div>
+      <DetailOrder orderId={order.id!} userId="1234test" />
     </div>
   );
 }
